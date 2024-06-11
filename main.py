7230f4ec41a1d040d7d9f79b0179cc019eeeed65
Felipe Falcon <felipealves0957@gmail.com>
2024-06-11 16:13:56 -0300

    Adding repeatability data

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class BlocoPadraoApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Bloco Padrão")
-        
+        self.background('#FFFFFF')
         self.data = {}
         
         self.label = tk.Label(root, text="Selecione um bloco padrão:")
@@ -22,11 +22,10 @@ class BlocoPadraoApp:
         self.text = tk.Text(root, height=10, width=50)
         self.text.pack(pady=10)
         
-        self.figure = plt.Figure(figsize=(8, 6), dpi=100)  # Aumentando o tamanho da figura
+        self.figure = plt.Figure(figsize=(8, 6), dpi=100)
         self.ax = self.figure.add_subplot(111)
         self.canvas = FigureCanvasTkAgg(self.figure, root)
         self.canvas.get_tk_widget().pack()
-        
         self.load_data()
     
     def load_data(self):
@@ -41,12 +40,13 @@ class BlocoPadraoApp:
             block_data = df[df['Bloco Padrão'] == c]['Medida'].tolist()
             block_mean = mean(block_data)
             block_trend = c - block_mean
-            block_stdev = 2.365 * pstdev(block_data)
+            block_stdev = pstdev(block_data)
             
             self.data[c] = {
                 'Média': f'{block_mean:.3f}',
                 'Tendência': block_trend,
                 'Desvio Padrão': block_stdev,
+                'Repetibilidade': 2.365 * block_stdev,
                 'Medidas': block_data
             }
         
@@ -57,26 +57,25 @@ class BlocoPadraoApp:
         if selected_block in self.data:
             block_info = self.data[selected_block]
             self.text.delete(1.0, tk.END)
-            self.text.insert(tk.END, f"Bloco Padrão: {selected_block}\n")
             self.text.insert(tk.END, f"Média: {block_info['Média']}\n")
             self.text.insert(tk.END, f"Tendência: {block_info['Tendência']:.3f}\n")
             self.text.insert(tk.END, f"Desvio Padrão: {block_info['Desvio Padrão']:.3f}\n")
+            self.text.insert(tk.END, f"Repetibilidade: {block_info['Repetibilidade']:.3f}\n")
             
             self.plot_data(block_info['Medidas'])
             
     def plot_data(self, measures):
+        selected_block = float(self.combo.get())
         self.ax.clear()
-        x_values = list(range(1, len(measures) + 1))  # Criar valores de x correspondentes ao índice das medidas
-        self.ax.plot(x_values, measures, marker='o')  # Usar os valores de x e as medidas
-        self.ax.set_title("Medidas")
+        x_values = list(range(1, len(measures) + 1))  
+        self.ax.plot(x_values, measures, marker='o')  
+        self.ax.set_title(f"Medidas para {selected_block:.3f} mm")
         self.ax.set_xlabel("Índice")
         self.ax.set_ylabel("Medida")
 
-        # Definir o formato dos números no eixo y
-        self.ax.yaxis.set_major_formatter(plt.FormatStrFormatter('%.3f'))  # Definir 3 casas decimais
+        self.ax.yaxis.set_major_formatter(plt.FormatStrFormatter('%.3f'))
 
         self.canvas.draw()
-
 
 if __name__ == "__main__":
     root = tk.Tk()
